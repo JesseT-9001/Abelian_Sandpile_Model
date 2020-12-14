eb44d54389e32a4989d351fa35ec6b448955a560
JesseTCS <jt873@scarletmail.rutgers.edu>
2020-12-14 22:20:45 +0000

    Finished basic input handling

--- project/main.cpp
+++ project/main.cpp
@@ -1,5 +1,6 @@
 #include <string>
 #include <vector>
+#include <iostream> // Here for testing purposes. Remove befor submittings.
 
 #include <ruc-sci-comp/animate.hpp>
 
@@ -8,5 +9,22 @@
 
 int main(int argc, char **argv)
 {
+    if (argc == 2)
+    {
+        // Make sandpile from file.
+        Sandpile sandpile = read_sandpile_file(argv[1]);
+    }
+    else if (argc == 3)
+    {
+        // Make sandpile using random generator.
+        int s = std::stoi(argv[1]);
+        int n = std::stoi(argv[2]);
+        Sandpile sandpile = create_sandpile(s, n);
 
+    }
+    
+    else
+    {
+        return 1;
+    }
 }
--- project/make_sandpile.cpp
+++ project/make_sandpile.cpp
@@ -9,5 +9,30 @@
 #include "make_sandpile.h"
 
 // define a function for generating a random sandpile given a size and count
+Sandpile create_sandpile(int s, int n)
+{
+    Row row(s, 0);
+    Sandpile sandpile(s, row);
+    std::mt19937_64 eng(1337);
+    std::uniform_int_distribution<int> dis(0, s-1);
+    for (int i = 0; i < n; i++)
+    {
+        sandpile[dis(eng)][dis(eng)] += 1;
+    }
+
+    return sandpile;
+}
 
 // define a function for reading a sandpile from a file given its filename
+Sandpile read_sandpile_file(char* file_name)
+{
+    std::ifstream f(file_name);
+    std::string buffer;
+    Sandpile sandpile;
+    while (std::getline(f, buffer))
+    {
+        std::istringstream ss(buffer);
+        std::copy(std::istream_iterator<int>(ss), {}, std::back_inserter(sandpile.emplace_back()));
+    }
+    return sandpile;
+}
